6d164e4ac47e9b7162b17d0bf2fe655c6a90dd97
byzaya <byzaya777@gmail.com>
2023-10-10 12:16:40 +0300

    fix style

--- src/main/java/org/example/entities/cart/Cart.java
+++ src/main/java/org/example/entities/cart/Cart.java
@@ -10,7 +10,6 @@ import org.example.entities.user.UserAccount;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Entity
 @Getter
 @Setter
--- src/main/java/org/example/entities/item/Item.java
+++ src/main/java/org/example/entities/item/Item.java
@@ -1,6 +1,5 @@
 package org.example.entities.item;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
