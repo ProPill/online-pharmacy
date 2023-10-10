0ce2de8023d50714e1987e39ec00a1cd23d3cc11
byzaya <byzaya777@gmail.com>
2023-10-10 12:08:38 +0300

    add example DTO

--- src/main/java/org/example/dto/item/TypeDto.java
+++ src/main/java/org/example/dto/item/TypeDto.java
@@ -1,4 +1,16 @@
 package org.example.dto.item;
 
-public record TypeDto() {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.example.entities.item.Type;
+
+public record TypeDto(
+        @JsonProperty("id") Long id,
+        @JsonProperty("id") String name
+) {
+    public static TypeDto fromType(Type type) {
+        return new TypeDto(
+                type.getId(),
+                type.getName()
+        );
+    }
 }
