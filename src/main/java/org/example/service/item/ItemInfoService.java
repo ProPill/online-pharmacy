7b3e329f70672643472535d34030836607e00059
byzaya <byzaya777@gmail.com>
2023-10-13 22:32:42 +0300

    implemented a service for getting info about item

--- src/main/java/org/example/service/item/ItemInfoService.java
+++ src/main/java/org/example/service/item/ItemInfoService.java
@@ -1,10 +1,25 @@
 package org.example.service.item;
 
+import lombok.RequiredArgsConstructor;
+import org.example.entities.item.Item;
+import org.example.repository.item.ItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import static org.example.exception.TypicalServerExceptions.ITEM_NOT_FOUND;
+
 @Service
+@RequiredArgsConstructor
 public class ItemInfoService {
 
-    // get info about item
+    private final ItemRepository itemRepository;
 
+    public Item getItemInfo(Long id) {
+        Optional<Item> item = itemRepository.findById(id);
+        if (item.isEmpty()) {
+            ITEM_NOT_FOUND.throwException();
+        }
+        return item.get();
+    }
 }
