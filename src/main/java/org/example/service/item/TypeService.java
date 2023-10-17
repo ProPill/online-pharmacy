627ba4e04d15fb1626dc8eb7f1966e863be8f1dc
byzaya <byzaya777@gmail.com>
2023-10-17 22:51:19 +0300

    add type service and controller

---
+++ src/main/java/org/example/service/item/TypeService.java
@@ -0,0 +1,19 @@
+package org.example.service.item;
+
+import lombok.RequiredArgsConstructor;
+import org.example.entities.item.Type;
+import org.example.repository.item.TypeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TypeService {
+
+  private static TypeRepository typeRepository;
+
+  public List<Type> getAll() {
+    return typeRepository.findAll();
+  }
+}
