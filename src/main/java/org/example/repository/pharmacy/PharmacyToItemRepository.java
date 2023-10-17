2ab4b2dc55fde75869b2a2ac31079953a7c9b765
byzaya <byzaya777@gmail.com>
2023-10-17 23:31:47 +0300

    implement all methods in PharmacyService

--- src/main/java/org/example/repository/pharmacy/PharmacyToItemRepository.java
+++ src/main/java/org/example/repository/pharmacy/PharmacyToItemRepository.java
@@ -1,8 +1,13 @@
 package org.example.repository.pharmacy;
 
+import java.util.List;
+import org.example.entities.item.Item;
+import org.example.entities.pharmacy.Pharmacy;
 import org.example.entities.pharmacy.PharmacyToItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PharmacyToItemRepository extends JpaRepository<PharmacyToItem, Long> {}
+public interface PharmacyToItemRepository extends JpaRepository<PharmacyToItem, Long> {
+  List<Pharmacy> findByItem(Item item);
+}
---
+++ src/main/java/org/example/service/pharmacy/PharmacyService.java
@@ -0,0 +1,34 @@
+package org.example.service.pharmacy;
+
+import static org.example.exception.TypicalServerExceptions.ITEM_NOT_FOUND;
+
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.example.entities.item.Item;
+import org.example.entities.pharmacy.Pharmacy;
+import org.example.repository.item.ItemRepository;
+import org.example.repository.pharmacy.PharmacyRepository;
+import org.example.repository.pharmacy.PharmacyToItemRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PharmacyService {
+
+  private final PharmacyRepository pharmacyRepository;
+  private final PharmacyToItemRepository pharmacyToItemRepository;
+  private final ItemRepository itemRepository;
+
+  public List<Pharmacy> getAll() {
+    return pharmacyRepository.findAll();
+  }
+
+  public List<Pharmacy> getAllByItemId(Long id) {
+    Optional<Item> item = itemRepository.findById(id);
+    if (item.isEmpty()) {
+      ITEM_NOT_FOUND.throwException();
+    }
+    return pharmacyToItemRepository.findByItem(item.get());
+  }
+}
