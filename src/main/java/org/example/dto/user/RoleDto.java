4f198689448d5aa6151d96a4c7a010fd43fad811
byzaya <byzaya777@gmail.com>
2023-10-17 15:47:48 +0300

    fix style

--- src/main/java/org/example/dto/user/RoleDto.java
+++ src/main/java/org/example/dto/user/RoleDto.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.example.entities.user.Role;
 
 public record RoleDto(@JsonProperty("id") Long id, @JsonProperty("name") String name) {
-    public static RoleDto fromRole(Role role) {
-        return new RoleDto(
-                role.getId(),
-                role.getName()
-        );
-    }
+  public static RoleDto fromRole(Role role) {
+    return new RoleDto(role.getId(), role.getName());
+  }
 }
