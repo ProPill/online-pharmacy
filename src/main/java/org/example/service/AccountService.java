f4aa257b6c0e31637d016a72817a56c83b4c2d14
LysenkoAleksandra <orange260302@gmail.com>
2023-11-05 17:36:10 +0300

    Add regex for registration fields validation

--- src/main/java/org/example/service/AccountService.java
+++ src/main/java/org/example/service/AccountService.java
@@ -1,8 +1,8 @@
 package org.example.service;
 
-import static org.example.exception.TypicalServerExceptions.WRONG_INPUT_DATA;
-
 import java.util.Optional;
+import java.util.regex.Pattern;
+
 import lombok.RequiredArgsConstructor;
 import org.example.entities.user.Role;
 import org.example.entities.user.UserAccount;
@@ -10,6 +10,8 @@ import org.example.repository.user.RoleRepository;
 import org.example.repository.user.UserAccountRepository;
 import org.example.security.PasswordEncryption;
 import org.springframework.stereotype.Service;
+
+import static org.example.exception.TypicalServerExceptions.*;
 
 @Service
 @RequiredArgsConstructor
@@ -20,28 +22,37 @@ public class AccountService {
   private final PasswordEncryption encryptor = new PasswordEncryption();
 
   public UserAccount register(String fullName, String phone, String password) {
+    String FIORegex = "^[А-ЯЁ][а-яё]*$";
+    String phoneNumberRegex = "^((\\+7)+([0-9]){10})$";
+    String passwordRegex = "^(([A-z0-9]){6,16})$";
+
+    Pattern patternFIO = Pattern.compile(FIORegex);
+    Pattern patternNumber=Pattern.compile(phoneNumberRegex);
+    Pattern patternPassword = Pattern.compile(passwordRegex);
+
     if (userAccountRepository.findByPhone(phone).isEmpty()) {
-      WRONG_INPUT_DATA.throwException();
+      PHONE_IS_REGISTERED.throwException();
     }
+    ////TODO: смотрим что введённые параметры не превышают лимиты, если хоть один превышает, то WRONG INPUT
     Optional<Role> role = roleRepository.findByName("пользователь");
     byte [] encryptedPassword = encryptor.getCiphertext(password);
     UserAccount newUser = new UserAccount();
     newUser.setFullName(fullName);
     newUser.setPhone(phone);
     newUser.setPasswordHash(encryptedPassword);
-    ////TODO: смотрим что с таким номером еще нет пчелов
     return userAccountRepository.save(newUser);
   }
 
   public UserAccount login(String phone, String password) {
     byte [] encryptedPassword = encryptor.getCiphertext(password);
-    if (userAccountRepository.findUserAccountByPhoneAndPasswordHash(phone, encryptedPassword).isEmpty()){
-
+    Optional<UserAccount> user = userAccountRepository.findUserAccountByPhoneAndPasswordHash(phone, encryptedPassword);
+    if (user.isEmpty()){
+      WRONG_LOGIN_PASSWORD.throwException();
     }
-
-
-
+    return user.get();
   }
 
-  public UserAccount logout(Long userId) {}
+  public UserAccount logout(Long userId) {
+    
+  }
 }
