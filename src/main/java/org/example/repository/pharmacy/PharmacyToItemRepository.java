24059e6dc6157eaacc54ad875caf563c66df45b1
byzaya <byzaya777@gmail.com>
2023-11-08 17:09:12 +0300

    style

--- src/main/java/org/example/repository/pharmacy/PharmacyToItemRepository.java
+++ src/main/java/org/example/repository/pharmacy/PharmacyToItemRepository.java
@@ -1,8 +1,5 @@
 package org.example.repository.pharmacy;
 
-import java.util.List;
-import org.example.entities.item.Item;
-import org.example.entities.pharmacy.Pharmacy;
 import org.example.entities.pharmacy.PharmacyToItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
