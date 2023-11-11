ce7f0b0c0634529ef44987e4dc0cbb4182a14153
byzaya <byzaya777@gmail.com>
2023-11-11 10:27:24 +0300

    fix code style

--- src/main/java/org/example/exception/TypicalServerExceptions.java
+++ src/main/java/org/example/exception/TypicalServerExceptions.java
@@ -17,7 +17,8 @@ public enum TypicalServerExceptions {
   WRONG_LOGIN_PASSWORD(
       new ServerException(HttpStatus.BAD_REQUEST, "WRONG_LOGIN_PASSWORD", "WRONG_LOGIN_PASSWORD")),
   USER_NOT_DOC(new ServerException(HttpStatus.BAD_REQUEST, "USER_NOT_DOC", "USER_NOT_DOC")),
-  PHARMACY_NOT_FOUND(new ServerException(HttpStatus.NOT_FOUND, "PHARMACY_NOT_FOUND", "PHARMACY_NOT_FOUND"));
+  PHARMACY_NOT_FOUND(
+      new ServerException(HttpStatus.NOT_FOUND, "PHARMACY_NOT_FOUND", "PHARMACY_NOT_FOUND"));
 
   private final ServerException serverException;
 
