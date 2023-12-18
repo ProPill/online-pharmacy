3a4aad65daa6920b69131061a2cfa6438e96eeca
LysenkoAleksandra <orange260302@gmail.com>
2023-12-18 21:24:41 +0300

    Fix style

--- src/main/java/org/example/repository/cart/CartRepository.java
+++ src/main/java/org/example/repository/cart/CartRepository.java
@@ -1,10 +1,9 @@
 package org.example.repository.cart;
 
+import java.util.Optional;
 import org.example.entities.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
