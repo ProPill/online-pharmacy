267b13a82128bc12dd260154cb327fbc8134e8b6
LysenkoAleksandra <orange260302@gmail.com>
2023-12-18 19:58:48 +0300

    Implement cart services

--- src/main/java/org/example/repository/cart/CartRepository.java
+++ src/main/java/org/example/repository/cart/CartRepository.java
@@ -4,5 +4,9 @@ import org.example.entities.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {}
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByUserAccount_Id(Long user_id);
+}
--- src/main/java/org/example/repository/cart/CartToItemRepository.java
+++ src/main/java/org/example/repository/cart/CartToItemRepository.java
@@ -1,8 +1,13 @@
 package org.example.repository.cart;
 
+import java.util.Optional;
 import org.example.entities.cart.CartToItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartToItemRepository extends JpaRepository<CartToItem, Long> {}
+public interface CartToItemRepository extends JpaRepository<CartToItem, Long> {
+  Optional<CartToItem> getCartToItemByItem_Id(Long item_id);
+
+  Optional<CartToItem> findByItem_Id(Long item_id);
+}
