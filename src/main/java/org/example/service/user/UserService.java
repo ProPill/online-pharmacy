e2da023b8a57d6e61c5aa22b6334163d4da6ff8d
LysenkoAleksandra <orange260302@gmail.com>
2023-12-19 15:05:06 +0300

    Add user controller

---
+++ src/main/java/org/example/service/user/UserService.java
@@ -0,0 +1,25 @@
+package org.example.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.example.entities.user.UserAccount;
+import org.example.repository.user.UserAccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static org.example.exception.TypicalServerExceptions.*;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserAccountRepository userAccountRepository;
+
+    public UserAccount getUserInfo(Long user_id) {
+
+        Optional<UserAccount> user = userAccountRepository.findById(user_id);
+        if (user.isEmpty()) {
+            USER_NOT_FOUND.throwException();
+        }
+        return user.get();
+    }
+}
