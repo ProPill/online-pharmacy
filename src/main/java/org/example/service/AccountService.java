a403d93ac8200d220221a35861fcb83383835e39
LysenkoAleksandra <orange260302@gmail.com>
2024-01-17 21:04:13 +0300

    Linter fixesx2

--- src/main/java/org/example/service/AccountService.java
+++ src/main/java/org/example/service/AccountService.java
@@ -10,7 +10,6 @@ import java.util.Arrays;
 import java.util.HexFormat;
 import java.util.Optional;
 import java.util.regex.Pattern;
-
 import lombok.RequiredArgsConstructor;
 import org.example.entities.user.Role;
 import org.example.entities.user.UserAccount;
