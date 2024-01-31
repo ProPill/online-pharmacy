cca4f05d44bebd2811a64f34f34890050ebd6e76
byzaya <byzaya777@gmail.com>
2024-01-31 22:36:17 +0300

    add jacoco

---
+++ src/test/java/org/example/service/pharmacy/PharmacyServiceTest.java
@@ -0,0 +1,15 @@
+package org.example.service.pharmacy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class PharmacyServiceTest {
+
+    @Test
+    void getAll() {
+      }
+
+    @Test
+    void getAllByItemId() {
+      }
+}
