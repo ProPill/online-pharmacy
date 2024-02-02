e65be7777cec302330fa511056bab1cc685c61f1
byzaya <byzaya777@gmail.com>
2024-02-02 15:03:14 +0300

    first try

--- src/test/java/org/example/service/pharmacy/PharmacyServiceTest.java
+++ src/test/java/org/example/service/pharmacy/PharmacyServiceTest.java
@@ -3,12 +3,28 @@ package org.example.service.pharmacy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.transaction.annotation.Transactional;
 
+@SpringJUnitConfig
+@SpringBootTest
+@Transactional
 class PharmacyServiceTest {
+
+  @Autowired
+  private PharmacyService pharmacyService;
 
   @Test
   void getAll() {}
 
   @Test
   void getAllByItemId() {}
+
+  @Test
+  void testGetAllByItemId_WhenItemNotFound() {
+    // Вызываем метод сервиса с несуществующим itemId
+    assertThrows(Exception.class, () -> pharmacyService.getAllByItemId(999L));
+  }
 }
