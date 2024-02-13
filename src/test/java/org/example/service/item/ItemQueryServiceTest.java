cdc93c8ddbfe8d97d00d0b386274202bf63d75de
byzaya <byzaya777@gmail.com>
2024-02-13 17:29:54 +0300

    add tests item query service

--- src/test/java/org/example/service/item/ItemQueryServiceTest.java
+++ src/test/java/org/example/service/item/ItemQueryServiceTest.java
@@ -3,13 +3,16 @@ package org.example.service.item;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import org.example.entities.item.Item;
 import org.example.entities.item.Type;
+import org.example.entities.user.Role;
 import org.example.entities.user.Speciality;
 import org.example.entities.user.UserAccount;
+import org.example.exception.ServerException;
 import org.example.repository.item.ItemRepository;
 import org.example.repository.item.TypeRepository;
 import org.example.repository.user.SpecialityRepository;
@@ -20,6 +23,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 @ExtendWith(MockitoExtension.class)
 class ItemQueryServiceTest {
@@ -71,11 +75,37 @@ class ItemQueryServiceTest {
     assertEquals(2, result.size());
   }
 
+  // TODO
   @Test
-  void getAllItemsByDocId_userNotFound() {}
+  void getAllItemsByDocId_userNotFound() {
+    Long userId = 1L;
+    when(userAccountRepository.findById(userId)).thenReturn(Optional.empty());
 
+    try {
+      itemQueryService.getAllItemsByDocId(userId);
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
+      assertEquals("USER_NOT_FOUND", e.getCode());
+      assertEquals("USER_NOT_FOUND", e.getMessage());
+    }
+  }
+
+  // TODO
   @Test
-  void getAllItemsByDocId_userNotDoc() {}
+  void getAllItemsByDocId_userNotDoc() {
+    UserAccount user = new UserAccount(1L, "name", "89370000000", new byte[]{}, new Role(), null,
+        new ArrayList<>(), new ArrayList<>());
+    Long userId = 1L;
+    when(userAccountRepository.findById(userId)).thenReturn(Optional.of(user));
+
+    try {
+      itemQueryService.getAllItemsByDocId(userId);
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
+      assertEquals("USER_NOT_DOC", e.getCode());
+      assertEquals("USER_NOT_DOC", e.getMessage());
+    }
+  }
 
   @Test
   void getAllItemsByTypeId() {
@@ -87,8 +117,20 @@ class ItemQueryServiceTest {
     assertEquals(2, result.size());
   }
 
+  // TODO
   @Test
-  void getAllItemsByTypeId_typeNotFound() {}
+  void getAllItemsByTypeId_typeNotFound() {
+    Long typeId = 1L;
+    when(typeRepository.findById(typeId)).thenReturn(Optional.empty());
+
+    try {
+      itemQueryService.getAllItemsByTypeId(typeId);
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
+      assertEquals("TYPE_NOT_FOUND", e.getCode());
+      assertEquals("TYPE_NOT_FOUND", e.getMessage());
+    }
+  }
 
   @Test
   void getAllItemsBySpecId() {
@@ -101,5 +143,16 @@ class ItemQueryServiceTest {
   }
 
   @Test
-  void getAllItemsBySpecId_specialityNotFound() {}
+  void getAllItemsBySpecId_specialityNotFound() {
+    Long specId = 1L;
+    when(specialityRepository.findById(specId)).thenReturn(Optional.empty());
+
+    try {
+      itemQueryService.getAllItemsBySpecId(specId);
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
+      assertEquals("SPECIALITY_NOT_FOUND", e.getCode());
+      assertEquals("SPECIALITY_NOT_FOUND", e.getMessage());
+    }
+  }
 }
