d3055228942baec41c39af68f3a72e88f5ad5c64
byzaya <byzaya777@gmail.com>
2024-02-13 17:51:22 +0300

    add type service test

--- src/test/java/org/example/service/item/TypeServiceTest.java
+++ src/test/java/org/example/service/item/TypeServiceTest.java
@@ -1,7 +1,13 @@
 package org.example.service.item;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.example.entities.item.Type;
 import org.example.repository.item.TypeRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,9 +21,20 @@ class TypeServiceTest {
 
   @InjectMocks private TypeService typeService;
 
-  @BeforeEach
-  void setUp() {}
-
   @Test
-  void getAll() {}
+  void getAll() {
+    List<Type> expectedTypes =
+        Arrays.asList(
+            new Type(1L,
+                "Type1",
+                new ArrayList<>()),
+            new Type(2L,
+                "Type2",
+                new ArrayList<>()));
+    when(typeRepository.findAll()).thenReturn(expectedTypes);
+    List<Type> result = typeService.getAll();
+    assertEquals(2, result.size());
+    assertEquals("Type1", result.get(0).getName());
+    assertEquals("Type2", result.get(1).getName());
+  }
 }
