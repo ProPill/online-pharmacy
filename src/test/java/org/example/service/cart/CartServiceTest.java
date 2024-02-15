5748c84cf8ff7930c19392b50f3c2a1d7810f696
LysenkoAleksandra <orange260302@gmail.com>
2024-02-15 19:04:44 +0300

    first tests for cart

--- src/test/java/org/example/service/cart/CartServiceTest.java
+++ src/test/java/org/example/service/cart/CartServiceTest.java
@@ -29,7 +29,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
