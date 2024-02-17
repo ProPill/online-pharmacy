9e37ba9f8f95af93943a7455722cbc1b0c561371
byzaya <byzaya777@gmail.com>
2024-02-17 21:09:48 +0300

    complete - 63 tests > 90% coverage

--- src/test/java/org/example/service/order/OrderServiceTest.java
+++ src/test/java/org/example/service/order/OrderServiceTest.java
@@ -1,6 +1,8 @@
 package org.example.service.order;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.sql.Date;
@@ -8,8 +10,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import org.example.entities.item.Item;
 import org.example.entities.order.Orders;
 import org.example.entities.pharmacy.Pharmacy;
+import org.example.entities.pharmacy.PharmacyToItem;
 import org.example.entities.user.UserAccount;
 import org.example.exception.ServerException;
 import org.example.repository.item.ItemRepository;
@@ -84,37 +88,111 @@ class OrderServiceTest {
     }
   }
 
-  //  @Test
-  //  void placeOrder() {
-  //    UserAccount user = new UserAccount();
-  //    when(userAccountRepository.findById(1L)).thenReturn(Optional.of(user));
-  //    Pharmacy pharmacy = new Pharmacy();
-  //    when(pharmacyRepository.findById(1L)).thenReturn(Optional.of(pharmacy));
-  //    when(itemRepository.findById(1L)).thenReturn(Optional.of(new Item()));
-  //    Orders order = orderService.placeOrder(1L, new Date(2024, 2, 1), new Date(2024, 2, 5),
-  // 100.0, 1L, new Long[]{1L, 2L, 3L});
-  //    assertNotNull(order);
-  //    verify(ordersRepository, times(1)).save(any(Orders.class));
-  //    verify(cartService, times(1)).deleteItemFromCart(eq(1L), anyLong());
-  //  }
+  @Test
+  void placeOrder() {
+    UserAccount user = new UserAccount();
+    when(userAccountRepository.findById(1L)).thenReturn(Optional.of(user));
+    Pharmacy pharmacy = new Pharmacy();
+    when(pharmacyRepository.findById(1L)).thenReturn(Optional.of(pharmacy));
+    Item item1 = new Item();
+    when(itemRepository.findById(1L)).thenReturn(Optional.of(item1));
+    Item item2 = new Item();
+    when(itemRepository.findById(2L)).thenReturn(Optional.of(item2));
+    Orders order =
+        orderService.placeOrder(
+            1L, new Date(2024, 2, 1), new Date(2024, 2, 5), 100.0, 1L, new Long[] {1L, 2L});
+    verify(userAccountRepository, times(1)).findById(1L);
+    verify(pharmacyRepository, times(1)).findById(1L);
+    verify(ordersRepository, times(1)).save(order);
+  }
 
   @Test
-  void placeOrder_UserNotFound() {}
+  void placeOrder_UserNotFound() {
+    when(userAccountRepository.findById(1L)).thenReturn(Optional.empty());
+    try {
+      orderService.placeOrder(
+          1L, new Date(2024, 2, 1), new Date(2024, 2, 5), 100.0, 1L, new Long[] {1L, 2L, 3L});
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
+      assertEquals("USER_NOT_FOUND", e.getCode());
+      assertEquals("USER_NOT_FOUND", e.getMessage());
+    }
+  }
 
   @Test
-  void placeOrder_PharmacyNotFound() {}
+  void placeOrder_PharmacyNotFound() {
+    UserAccount user = new UserAccount();
+    when(userAccountRepository.findById(1L)).thenReturn(Optional.of(user));
+    when(pharmacyRepository.findById(1L)).thenReturn(Optional.empty());
+    try {
+      orderService.placeOrder(
+          1L, new Date(2024, 2, 1), new Date(2024, 2, 5), 100.0, 1L, new Long[] {1L, 2L, 3L});
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
+      assertEquals("PHARMACY_NOT_FOUND", e.getCode());
+      assertEquals("PHARMACY_NOT_FOUND", e.getMessage());
+    }
+  }
 
-  //  @Test
-  //  void placeOrder_ItemNotFound() {
-  //    UserAccount user = new UserAccount();
-  //    when(userAccountRepository.findById(1L)).thenReturn(Optional.of(user));
-  //    Pharmacy pharmacy = new Pharmacy();
-  //    when(pharmacyRepository.findById(1L)).thenReturn(Optional.of(pharmacy));
-  //    when(itemRepository.findById(1L)).thenReturn(Optional.of(new Item()));
-  //    Orders order = orderService.placeOrder(1L, new Date(2024, 2, 1), new Date(2024, 2, 5),
-  // 100.0, 1L, new Long[]{1L, 2L, 3L});
-  //    assertNotNull(order);
-  //    //verify(ordersRepository, times(1)).save(any(Orders.class));
-  //    //verify(cartService, times(1)).deleteItemFromCart(eq(1L), anyLong());
-  //  }
+  @Test
+  void placeOrder_ItemNotFound() {
+    UserAccount user = new UserAccount();
+    when(userAccountRepository.findById(1L)).thenReturn(Optional.of(user));
+    Pharmacy pharmacy = new Pharmacy();
+    when(pharmacyRepository.findById(1L)).thenReturn(Optional.of(pharmacy));
+    when(itemRepository.findById(1L)).thenReturn(Optional.of(new Item()));
+    try {
+      orderService.placeOrder(
+          1L, new Date(2024, 2, 1), new Date(2024, 2, 5), 100.0, 1L, new Long[] {1L, 2L, 3L});
+    } catch (ServerException e) {
+      assertEquals(HttpStatus.NOT_FOUND, e.getStatus());
+      assertEquals("ITEM_NOT_FOUND", e.getCode());
+      assertEquals("ITEM_NOT_FOUND", e.getMessage());
+    }
+  }
+
+  @Test
+  void testPlaceOrder_ItemFoundInPharmacyToItem() {
+    Long itemId = 123L;
+    Item item = new Item();
+    UserAccount user = new UserAccount();
+    when(userAccountRepository.findById(1L)).thenReturn(Optional.of(user));
+    Pharmacy pharmacy = new Pharmacy();
+    when(pharmacyRepository.findById(1L)).thenReturn(Optional.of(pharmacy));
+    when(itemRepository.findById(itemId)).thenReturn(Optional.of(item));
+    PharmacyToItem pharmacyToItem = new PharmacyToItem();
+    pharmacyToItem.setItem(item);
+    pharmacyToItem.setQuantity(1);
+    when(pharmacyToItemRepository.findAllByPharmacyIdAndItemId(1L, itemId))
+        .thenReturn(pharmacyToItem);
+    Orders order =
+        orderService.placeOrder(
+            1L, new Date(2024, 2, 1), new Date(2024, 2, 5), 100.0, 1L, new Long[] {itemId});
+    verify(itemRepository, times(4)).findById(itemId);
+    verify(pharmacyToItemRepository, times(1)).findAllByPharmacyIdAndItemId(1L, itemId);
+    verify(pharmacyToItemRepository, times(1)).save(pharmacyToItem);
+  }
+
+  @Test
+  void testPlaceOrder_ItemToQuantityMap() {
+    Long userId = 1L;
+    Item item1 = new Item();
+    Item item2 = new Item();
+    Date creationDate = new Date(2024, 2, 1);
+    Date deliveryDate = new Date(2024, 2, 5);
+    Double sumPrice = 100.0;
+    Long pharmacyId = 2L;
+    Long[] items = new Long[] {1L, 2L, 1L};
+    UserAccount user = new UserAccount();
+    when(itemRepository.findById(1L)).thenReturn(Optional.of(item1));
+    when(itemRepository.findById(2L)).thenReturn(Optional.of(item2));
+    when(userAccountRepository.findById(userId)).thenReturn(Optional.of(user));
+    Pharmacy pharmacy = new Pharmacy();
+    when(pharmacyRepository.findById(pharmacyId)).thenReturn(Optional.of(pharmacy));
+    Orders order =
+        orderService.placeOrder(userId, creationDate, deliveryDate, sumPrice, pharmacyId, items);
+    verify(userAccountRepository, times(1)).findById(userId);
+    verify(pharmacyRepository, times(1)).findById(pharmacyId);
+    verify(ordersRepository, times(1)).save(order);
+  }
 }
