727b237ce84d2e2f9e0bbb605dfe0180f89dbfce
byzaya <byzaya777@gmail.com>
2024-03-04 15:02:56 +0300

    add first test

--- src/test/java/org/example/controller/item/ItemInfoControllerTest.java
+++ src/test/java/org/example/controller/item/ItemInfoControllerTest.java
@@ -1,8 +1,14 @@
 package org.example.controller.item;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.example.entities.item.Item;
+import org.example.entities.item.Type;
+import org.example.entities.user.Speciality;
 import org.example.service.item.ItemInfoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,8 +22,38 @@ class ItemInfoControllerTest {
 
   @Autowired private MockMvc mockMvc;
 
-  @MockBean private ItemInfoService iteminfoService;
+  @MockBean private ItemInfoService itemInfoService;
+
+  private Item expectedItem;
+
+  @BeforeEach
+  void setUp() {
+    Type type = new Type();
+    type.setId(1L);
+    type.setName("Test Type");
+
+    Speciality speciality = new Speciality();
+    speciality.setId(1L);
+    speciality.setName("Test Speciality");
+
+    expectedItem = new Item();
+    expectedItem.setId(1L);
+    expectedItem.setName("Test Item");
+    expectedItem.setPrice(100.0);
+    expectedItem.setManufacturer("Test Manufacturer");
+    expectedItem.setInfo("Test Info");
+    expectedItem.setPictureUrl("test.jpg");
+    expectedItem.setType(type);
+    expectedItem.setSpeciality(speciality);
+  }
 
   @Test
-  void getItemInfo() {}
+  void getItemInfo() throws Exception {
+    // Устанавливаем поведение mock сервиса itemInfoService
+    when(itemInfoService.getItemInfo(1L)).thenReturn(expectedItem);
+
+    // Выполняем запрос к ендпоинту и проверяем статус ответа
+    mockMvc.perform(get("/api/item/info/1"))
+        .andExpect(status().isOk());
+  }
 }
