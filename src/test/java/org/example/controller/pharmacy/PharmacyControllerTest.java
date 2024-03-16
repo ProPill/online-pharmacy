9d8635382f7ad4f813b06b8606b6529ef766f4eb
byzaya <byzaya777@gmail.com>
2024-03-16 22:23:39 +0300

    delete extra imports

--- src/test/java/org/example/controller/pharmacy/PharmacyControllerTest.java
+++ src/test/java/org/example/controller/pharmacy/PharmacyControllerTest.java
@@ -1,10 +1,8 @@
 package org.example.controller.pharmacy;
 
 import static org.example.controller.TestObjects.pharmacies;
-import static org.example.exception.TypicalServerExceptions.ITEM_NOT_FOUND;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
