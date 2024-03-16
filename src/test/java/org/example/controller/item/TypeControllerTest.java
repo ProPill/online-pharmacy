7ecc4f934ae3eb68d6a9e548c58dd990e878ab1a
LysenkoAleksandra <orange260302@gmail.com>
2024-03-16 16:20:24 +0300

    verify json in type controller test

--- src/test/java/org/example/controller/item/TypeControllerTest.java
+++ src/test/java/org/example/controller/item/TypeControllerTest.java
@@ -6,17 +6,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import lombok.SneakyThrows;
 import org.example.entities.item.Type;
+import org.example.entities.pharmacy.Pharmacy;
 import org.example.service.item.TypeService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,14 +30,31 @@ class TypeControllerTest {
 
   @MockBean private TypeService typeService;
 
+  private Type firstType;
+  private Type secondType;
+  private List<Type> types;
+  @BeforeEach
+  void setUp() {
+    firstType = new Type(1L,"специальный", new ArrayList<>());
+    secondType = new Type(2L,"по рецепту", new ArrayList<>());
+
+    types = Arrays.asList(firstType, secondType);
+  }
+
+  private static void compare(ResultActions resultActions, String prefix, Type type) throws Exception {
+    resultActions.andExpectAll(
+            jsonPath(prefix + ".id").value(type.getId()),
+            jsonPath(prefix + ".name").value(type.getName()));
+  }
   @Test
   @SneakyThrows
   void getAll() {
-    List<Type> types = Arrays.asList(new Type(), new Type());
     when(typeService.getAll()).thenReturn(types);
-    mockMvc
+    ResultActions result= mockMvc
         .perform(get("/api/type/all"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$", hasSize(2)));
+        .andExpectAll(status().isOk(),
+            jsonPath("$", hasSize(2)));
+    compare(result, "$[0]", firstType);
+    compare(result, "$[1]", secondType);
   }
 }
