66c6245515030a5fcce3a873e498cd721f4ce5c6
byzaya <byzaya777@gmail.com>
2024-03-17 00:46:34 +0300

    add errors tests

--- src/test/java/org/example/controller/cart/CartControllerTest.java
+++ src/test/java/org/example/controller/cart/CartControllerTest.java
@@ -1,5 +1,15 @@
 package org.example.controller.cart;
 
+import static org.example.controller.TestObjects.notFound;
+import static org.example.controller.TestObjects.notFoundCode;
+import static org.example.controller.TestObjects.userNotFound;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import lombok.SneakyThrows;
 import org.example.controller.MvcUtil;
 import org.junit.jupiter.api.Test;
@@ -24,6 +34,12 @@ class CartControllerTest {
   @Test
   @SneakyThrows
   void getAllItemsInCart_UserNotFound() {
+    mockMvc
+        .perform(get("/api/cart/-1"))
+        .andExpectAll(status().isNotFound(), jsonPath("$.*", hasSize(3)))
+        .andExpect(jsonPath("$.code").value(userNotFound))
+        .andExpect(jsonPath("$.status").value(notFoundCode))
+        .andExpect(jsonPath("$.message").value(userNotFound));
   }
 
   @Test
@@ -34,11 +50,33 @@ class CartControllerTest {
   @Test
   @SneakyThrows
   void addItemToCart_UserNotFound() {
+    mockMvc
+        .perform(
+            post("/api/cart/add")
+                .param("item_id", "1")
+                .param("user_id", "-1")
+                .param("count", "1")
+        )
+        .andExpectAll(status().isNotFound(), jsonPath("$.*", hasSize(3)))
+        .andExpect(jsonPath("$.code").value(userNotFound))
+        .andExpect(jsonPath("$.status").value(notFoundCode))
+        .andExpect(jsonPath("$.message").value(userNotFound));
   }
 
   @Test
   @SneakyThrows
   void addItemToCart_ItemNotFound() {
+    mockMvc
+        .perform(
+            post("/api/cart/add")
+                .param("item_id", "-1")
+                .param("user_id", "1")
+                .param("count", "1")
+        )
+        .andExpectAll(status().isNotFound(), jsonPath("$.*", hasSize(3)))
+        .andExpect(jsonPath("$.code").value(notFound))
+        .andExpect(jsonPath("$.status").value(notFoundCode))
+        .andExpect(jsonPath("$.message").value(notFound));
   }
 
   @Test
@@ -49,5 +87,15 @@ class CartControllerTest {
   @Test
   @SneakyThrows
   void deleteItemFromCart_UserNotFound() {
+    mockMvc
+        .perform(
+            delete("/api/cart/delete")
+                .param("item_id", "1")
+                .param("user_id", "-1")
+        )
+        .andExpectAll(status().isNotFound(), jsonPath("$.*", hasSize(3)))
+        .andExpect(jsonPath("$.code").value(userNotFound))
+        .andExpect(jsonPath("$.status").value(notFoundCode))
+        .andExpect(jsonPath("$.message").value(userNotFound));
   }
 }
